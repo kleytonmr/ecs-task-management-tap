dd948ac65c1ea8fffac6dcf33e139669e66e426c
Kleytonmr <kleytonmatosramos@gmail.com>
2024-06-22 20:55:37 -0300

    version update

--- ecs-task-management.rb
+++ ecs-task-management.rb
@@ -1,13 +1,14 @@
 class EcsTaskManagement < Formula
   desc "Interactive Task Management in Amazon ECS"
   homepage "https://github.com/kleytonmr/ecs-task-management"
-  url "https://github.com/kleytonmr/ecs-task-management/releases/download/v1.0.2/ecs-task-management-1.0.2.zip"
-  sha256 "7ff364fcfa8d5b7687e47408161d08cc08813be6419604822d5bf05924759e1e"
+  url "https://github.com/kleytonmr/ecs-task-management/releases/download/v1.0.3/ecs-task-management-1.0.3.zip"
+  sha256 "4d7afadb9ff0fa90b93ab411c913073a0cdce04af28247f8aa6ed455f1f233e5"
 
   license "MIT"
 
   def install
     bin.install "ecs_tasks.sh" => "ecs-task-management"
+    prefix.install "translations"
   end
 
   test do
