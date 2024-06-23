2c46333d9c3168f5c6cb8d61c0e9a9d7f5c46e51
Kleytonmr <kleytonmatosramos@gmail.com>
2024-06-23 13:26:54 -0300

    release update

--- ecs-task-management.rb
+++ ecs-task-management.rb
@@ -1,14 +1,21 @@
 class EcsTaskManagement < Formula
   desc "Interactive Task Management in Amazon ECS"
   homepage "https://github.com/kleytonmr/ecs-task-management"
-  url "https://github.com/kleytonmr/ecs-task-management/releases/download/v1.0.3/ecs-task-management-1.0.3.zip"
-  sha256 "4d7afadb9ff0fa90b93ab411c913073a0cdce04af28247f8aa6ed455f1f233e5"
+  url "https://github.com/kleytonmr/ecs-task-management/releases/download/v1.0.4/ecs-task-management-1.0.4.zip"
+  sha256 "ace412d910fbdf615bc553fdbe064ed8bc8251c333ee6b7b24ed0a507c71e935"
 
   license "MIT"
 
   def install
     bin.install "ecs_tasks.sh" => "ecs-task-management"
     prefix.install "translations"
+  end
+
+  def caveats
+    <<~EOS
+      To use ecs-task-management, you need to set the JSON_FILE_PATH environment variable to the location of the translations directory:
+        export JSON_FILE_PATH=#{opt_prefix}/translations
+    EOS
   end
 
   test do
