eec4840998103268dfce0db62d284078bd6659d8
Kleytonmr <kleytonmatosramos@gmail.com>
2024-07-03 14:10:36 -0300

    update version

--- ecs-task-management.rb
+++ ecs-task-management.rb
@@ -1,8 +1,8 @@
 class EcsTaskManagement < Formula
   desc "Interactive Task Management in Amazon ECS"
   homepage "https://github.com/kleytonmr/ecs-task-management"
-  url "https://github.com/kleytonmr/ecs-task-management/releases/download/v1.0.7/ecs-task-management-1.0.7.zip"
-  sha256 "9981e2ef69ea72643d6e9acb2844af873617e661fa8bfd7d6a7fa9902beefdec"
+  url "https://github.com/kleytonmr/ecs-task-management/releases/download/v1.0.8/ecs-task-management-1.0.8.zip"
+  sha256 "245080e229cf16bacfce0f4b3d182329d94a461dc99a4cd846a975c9a3d753e3"
 
   license "MIT"
 
